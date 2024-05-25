dd90dcfcdae688772939ddd0c4b06f2ea47b83b8
ravinduyas <ravindu.yasanka21@gmail.com>
2024-05-25 12:33:48 +0530

    market items adding

--- Flask/Flask Market/main.py
+++
@@ -1,9 +0,0 @@
-#Running on http://127.0.0.1:5000
-
-from flask import Flask, render_template
-app =Flask(__name__)
-
-@app.route('/')
-@app.route('/home')
-def home_page():
-    return render_template('home.html')
