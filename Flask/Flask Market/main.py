06654903986227186dcc5f28334454ee9dab0984
ravinduyas <ravindu.yasanka21@gmail.com>
2024-05-25 10:44:07 +0530

    variable url

--- Flask/Flask Market/main.py
+++ Flask/Flask Market/main.py
@@ -1,6 +1,14 @@
 from flask import Flask
 app =Flask(__name__)
 
-@app.route('/')
+#Running on http://127.0.0.1:5000
+
+@app.route('/hello')
 def hello_world():
-    return 'Hello, World!'
+    return '''
+    <h1>Hello, World!!!</h1>
+    '''
+@app.route('/about/<username>')
+
+def about_page(username):
+    return f'<h1>This is the about page of {username}</h1>'
