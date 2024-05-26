c1cb18a9e121b98c47954c7ff148a863a3b88b7e
ravinduyas <ravindu.yasanka21@gmail.com>
2024-05-27 03:11:48 +0530

    sql alchemy error solved

--- Flask/Flask Market/market.py
+++ Flask/Flask Market/market.py
@@ -1,18 +1,19 @@
-
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///market.db'
-db=SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 class Item(db.Model):
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(length=30), nullable=False, unique=True )
-    price = db.Column(db.Integer(), nullable=False)
-    barcode =db.Column(db.String(length=12), nullable=False, unique=True)
-    description = db.Column(db.String(length=1024),nullable=False, unique=True)
-    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
+    barcode = db.Column(db.String(12), nullable=False, unique=True)
+    description = db.Column(db.String(1024), nullable=False, unique=True)
+
 @app.route('/')
 @app.route('/home')
 def home_page():
@@ -20,9 +21,11 @@ def home_page():
 
 @app.route('/market')
 def market_page():
-    items = [
-        {'id': 1, 'name': 'Phone', 'barcode': '893212299897', 'price': 500},
-        {'id': 2, 'name': 'Laptop', 'barcode': '123985473165', 'price': 900},
-        {'id': 3, 'name': 'Keyboard', 'barcode': '231985128446', 'price': 150}
-    ]
+    items = Item.query.all()
     return render_template('market.html', items=items)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Create database tables within the application context
+    
+    app.run(debug=True)
