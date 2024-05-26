1c10fe3fb0337c57bf1e8e7f831c2e3585e95911
ravinduyas <ravindu.yasanka21@gmail.com>
2024-05-26 22:50:18 +0530

    sql alchedmy

--- Flask/Flask Market/market.py
+++ Flask/Flask Market/market.py
@@ -1,11 +1,13 @@
+
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLAlchemy_DATABASE_URI'] = sqllite:///market.db
+app.config['SQLAlchemy_DATABASE_URI'] = 'sqlite:///market.db'
 db=SQLAlchemy(app)
 
 class Item(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True )
     price = db.Column(db.Integer(), nullable=False)
     barcode =db.Column(db.String(length=12), nullable=False, unique=True)
