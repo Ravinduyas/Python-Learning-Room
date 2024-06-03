0742bb943ab7c8dd5f31386d93350b3748b73296
ravinduyas <ravindu.yasanka21@gmail.com>
2024-06-03 23:36:05 +0530

    mvc architecture removed

--- search-by-id/main.py
+++
@@ -1,7 +0,0 @@
-if __name__ == "__main__":
-    root = tk.Tk()
-    model = FileSearchModel()
-    view = FileSearchView(root, None)  # will set the controller
-    controller = FileSearchController(model, view)
-    view.controller = controller  # Set the controller for the view
-    root.mainloop()
